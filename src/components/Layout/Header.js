4f3f1e1550355f66883b811276ab73d5ae0979d7
Chrispaix KAZE <kchrispaixs@gmail.com>
2023-04-26 11:44:49 +0200

    Add a modal via react portal, add cart and modal state, and add cart context

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     <Fragment>
       <header className={classes.header}>
         <h1>Foody</h1>
-        <HeaderCardButton/>
+        <HeaderCardButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt="a table full of delicious food" />
